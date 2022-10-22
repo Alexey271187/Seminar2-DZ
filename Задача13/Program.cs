63c0aca2ff2177f872a6e0c41a287a7b908d1115
Alexey <brazhnikovas_87@bk.ru>
2022-10-22 13:27:01 +0500

    13

---
+++ Задача13/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Впишите число");
+int numberA = int.Parse(Console.ReadLine()!);
+int SearchTreeNumber(int numberA)
+{  
+if(numberA > 99)
+    {
+        
+        while (numberA > 1000)
+        numberA = numberA / 10;
+        {
+            int numberB = numberA % 10;
+            return numberB;
+        }
+    }
+else Console.WriteLine("Третья цифра в числе отсутствует");
+return numberA;
+}
+
+int numberC = SearchTreeNumber(numberA);
+
+Console.WriteLine(numberC);
