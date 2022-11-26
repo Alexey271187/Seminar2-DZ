25dbe27d1ea9163eef3c2d320768d55e1fe9b8d6
Alexey <brazhnikovas_87@bk.ru>
2022-11-26 07:05:13 +0500

    56

---
+++ Задача54/Program.cs
@@ -0,0 +1,71 @@
+﻿//Задайте двумерный массив. 
+//Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.WriteLine("Введите количество строк");
+int strings = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов");
+int columns = int.Parse(Console.ReadLine()!);
+int[,] numbers = new int[strings, columns];
+
+
+void FillArrayRandom(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    Console.WriteLine("Массив случайных чисел:");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+//Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+void OrderedArray(int[,] array)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+        {
+            for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+            {
+                if (numbers[i, z] < numbers[i, z + 1])
+                {
+                    int temp = 0;
+                    temp = numbers[i, z];
+                    numbers[i, z] = numbers[i, z + 1];
+                    numbers[i, z + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray2(int[,] array)
+{
+    Console.WriteLine("Упорядоченный массив:");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArrayRandom(numbers);
+PrintArray(numbers);
+OrderedArray(numbers);
+PrintArray2(numbers);
---
+++ Задача56/Program.cs
@@ -0,0 +1,61 @@
+﻿Console.WriteLine("Введите количество строк");
+int strings = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов");
+int columns = int.Parse(Console.ReadLine()!);
+int[,] numbers = new int[strings, columns];
+
+
+void FillArrayRandom(int[,] array) 
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+void MinStringSumElements(int[,] array)
+{
+    int minString = 0;
+    int minSumString = 0;
+    int sumString = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minString += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumString += array[i, j];
+        if (sumString < minString)
+        {
+            minString = sumString;
+            minSumString = i;
+        }
+        sumString = 0;
+    }
+    Console.WriteLine($"В строке {minSumString + 1} наименьшая сумма элементов");
+}
+
+FillArrayRandom(numbers);
+if (strings != columns) // Задайте прямоугольный двумерный массив. 
+{
+    PrintArray(numbers);
+    MinStringSumElements(numbers);
+}
+else Console.WriteLine("Количество строк и столбцов должно быть разным!");
